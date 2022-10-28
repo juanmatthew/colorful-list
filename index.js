87aec115603e7e0528b87b28f6d243b263a1412a
juanmatthew <101286477+JuaniMatthew@users.noreply.github.com>
2022-10-28 11:54:15 -0400

    Updated html to have button in note container, updated some of the class names, and started adding functions to the js file

---
+++ index.js
@@ -0,0 +1,36 @@
+
+//reference to note section application container
+const noteSection = document.getElementByID("note-container");
+
+//reference to add new note button
+const addNewNoteBtn = noteSection.querySelector(".add-new-note-btn");
+
+//to retrieve saved notes
+function getSavedNotes() {
+
+}
+
+//to save notes with notes array
+function saveNotes(notes) {
+    
+}
+
+//building a new html element - a note
+function buildNoteElement (id, content){
+
+}
+
+//to add new note and save it to local storage
+function addNewNote() {
+
+}
+
+//to update note without creating a new one
+function updateNote(id, newNote) {
+    
+}
+
+//to delete the note
+function deletedNotes(id, element) {
+    
+}
