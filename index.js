1d8ea63ec03797ae45cbf3197b29264b0be58553
juanmatthew <101286477+JuaniMatthew@users.noreply.github.com>
2022-10-28 17:38:08 -0400

    Updated the js so that the note appears and can be deleted. Updated some of the class names and changed the header text

--- index.js
+++ index.js
@@ -1,46 +1,51 @@
 
-//reference to note section application container
-const noteSection = document.getElementByID("note-container");
-
+//reference to notes container
+const notesContainer = document.getElementById("app");
 //reference to add new note button
-const addNewNoteBtn = noteSection.querySelector(".add-new-note-btn");
+const addNoteBtn = notesContainer.querySelector(".add-note-btn");
+
 
 //to display all notes saved for user
 getSavedNotes().forEach(note => {
+
     const noteElement = buildNoteElement(note.id, note.content);
-    
+
+    notesContainer.insertBefore(noteElement, addNoteBtn);
+
 });
 
-addNewNoteBtn.addEventListener("click", () => addNewNote());
+//add note button event listener
+addNoteBtn.addEventListener("click", () => addNote());
 
 
 //to retrieve saved notes
 function getSavedNotes() {
-    return JSON.parse(localStorage.getItem("stickynotes") || "[]");
+    return JSON.parse(localStorage.getItem ("stickynotes") || "[]");
 };
 
-//to save notes with notes array
+//to save the notes on local storage
 function saveNotes(notes) {
     localStorage.setItem("stickynotes", JSON.stringify(notes));
 };
 
 //building a new html element - a note
-function buildNoteElement (id, content){
-    //const elementTitle = noteSection.createElement("input")
-
+function buildNoteElement(id, content) {
     const element = document.createElement("textarea");
+
     element.classList.add("note");
     element.value = content;
-    element.placeholder = "Description...";
+    element.placeholder = "Note it!";
 
+    //event listener to save updated note to local storage
     element.addEventListener("change", () => {
         updateNote(id, element.value);
     });
 
-    element.addEventListener("dbclick", () => {
+    //event listener for double click to delete
+    element.addEventListener("dblclick", () => {
         const toDelete = confirm("Do you wish to delete this note?");
 
-        if(toDelete) {
+        if (toDelete){
             deletedNotes(id, element);
         };
     });
@@ -48,33 +53,37 @@ function buildNoteElement (id, content){
     return element;
 };
 
-//to add new note and save it to local storage
-function addNewNote() {
+//to add notes and store them
+function addNote() {
     const notes = getSavedNotes();
+
     const noteObject = {
         id: Math.floor(Math.random() * 100000),
-        content: ""
+        content:""
     };
 
     const noteElement = buildNoteElement(noteObject.id, noteObject.content);
 
+    notesContainer.insertBefore(noteElement, addNoteBtn);
+
     notes.push(noteObject);
     saveNotes(notes);
+
 };
 
-//to update note without creating a new one
-function updateNote(id, newNote) {
+//to update note without replacing it
+function updateNote(id, newContent) {
     const notes = getSavedNotes();
     const targetNote = notes.filter(note => note.id == id)[0];
 
-    targetNote.content = newNote;
+    targetNote.content = newContent;
     saveNotes(notes);
 };
 
-//to delete the note
+//to delete note
 function deletedNotes(id, element) {
-    const notes = getSavedNotes().filter(note => note.id != id);
-
+    const notes =  getSavedNotes().filter(note => note.id != id); 
+    
     saveNotes(notes);
-    noteSection.removeChild(element);
+    notesContainer.removeChild(element);
 };
